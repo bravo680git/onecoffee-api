a2679e4ded99d5770945fd628b3675ed41fa5fcd
Nguyen Quoc Nhien <nhien.nq@cyberlogitec.com>
2024-10-13 18:18:18 +0700

    re-order category list

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -11,7 +11,7 @@ export class CategoryService {
   }
 
   findAll() {
-    return this.prisma.category.findMany({ orderBy: { updatedAt: 'desc' } });
+    return this.prisma.category.findMany({ orderBy: { id: 'asc' } });
   }
 
   async findOne(id: number) {
