5ded04bff24bb4bd5793b80947ea26a2cf8e36f1
bravo <iambravo680@gmail.com>
2026-01-17 17:27:23 +0700

    feat: configure S3 client region

--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -11,6 +11,7 @@ export class FileService {
 
   constructor() {
     this.client = new S3Client({
+      region: 'us-east-1',
       credentials: {
         accessKeyId: process.env.S3_ACCESS_KEY_ID ?? '',
         secretAccessKey: process.env.S3_SECRET_ACCESS_KEY ?? '',
